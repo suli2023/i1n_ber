7c30203cbd1324af7205326ce8708a59543d7397
Tanar <erosistvan@zold.lan>
2023-12-20 10:04:26 +0100

    Az első feladat kész

---
+++ dolgozo.py
@@ -0,0 +1,12 @@
+#  név, település, cím, születés és fizetés
+# Tároló osztály
+# egy dolgozó tulajdonságainak tárolása
+
+class Dolgozo:
+    def __init__(self, nev, telepules, cim, szuletes, fizetes):
+        self.nev = nev
+        self.telepules = telepules
+        self.cim = cim
+        self.szuletes = szuletes
+        self.fizetes = fizetes
+
---
+++ main.py
@@ -0,0 +1,46 @@
+# Az alkalmazás itt fut
+
+from dolgozo import Dolgozo
+
+class Ber:
+    # Konstruktor
+    # Az osztályváltozóknak kezdő értéket adunk
+    # Adattagok előkészítése
+    def __init__(self):
+        self.filename = "berkft.txt"
+        self.dolgozoLista = []
+    
+    # fájl beolvasása:
+    def olvas_fajl(self):
+        fp = open(self.filename, "r", encoding="utf-8")
+        lines = fp.readlines()
+        fp.close()
+        for line in lines :
+            line = line.rstrip()
+            (nev, telepules, cim, szuletes, fizetes) = line.split(":")
+            dolgozo = Dolgozo(nev, telepules, cim, szuletes, fizetes)
+            self.dolgozoLista.append(dolgozo) 
+    
+    # Szolnokiak
+    def szolnoki(self):
+        szolnokiLista = []
+        for dolgozo in self.dolgozoLista:
+            if dolgozo.telepules == "Szolnok":
+                szolnokiLista.append(dolgozo)
+        # for vége
+        # legfiatalabb; minimumkiválasztás tétel
+        max_szolnoki = szolnokiLista[0]
+        for szolnoki in szolnokiLista:
+            if szolnoki.szuletes > max_szolnoki.szuletes:
+                max_szolnoki = szolnoki
+        
+        print(max_szolnoki.szuletes)
+        
+    
+    
+# class Ber vége
+
+ber = Ber()
+ber.olvas_fajl()
+ber.szolnoki()
+
